bce0ff9d894159b74a754043ef97e1f5ed301371
rkri02 <rkri02@naver.com>
2021-10-11 13:29:34 +0900

    기본적인 연산코드3

---
+++ ex4-3.js
@@ -0,0 +1,20 @@
+let a = 10; 
+let b = 5; 
+
+//c가 참이니 true
+c = a > b;
+console.log('c = a > b : ', c);
+
+//c가 거짓이니 false
+c = a < b;
+console.log('c = a < b : ', c);
+
+//c가 거짓이니 false
+c =  a == b;
+console.log('c = a == b : ', c);
+
+
+//c가 참이니 true
+c =  a != b;
+console.log('c = a != b : ', c);
+
