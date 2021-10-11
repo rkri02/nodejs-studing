848907f945578971e20dc1505992c9de19727226
rkri02 <rkri02@naver.com>
2021-10-11 10:01:07 +0900

    const에 대한 이해를 간략하게 설명

---
+++ ex3-2.js
@@ -0,0 +1,12 @@
+//const 는 상수선언 이며 값을 변경할수 없다
+//상수명을 정의할때 대문자로 정의 한다
+
+
+//fruit1은 바나나란 변수를 가지지만
+//오랜지로 다시 선언을해서 최종적으로는 오랜지가 나온다.
+let fruit1 = 'banana';
+fruit1 = 'orange';
+const pi = 3.14;
+
+console.log('fruit1 : ', fruit1);
+console.log('pi : ', pi);
