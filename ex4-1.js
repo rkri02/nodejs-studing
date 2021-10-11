9f994a69bd4287117175b54c3848c631eaed965b
rkri02 <rkri02@naver.com>
2021-10-11 13:28:38 +0900

    기본적인 연산코드

---
+++ ex4-1.js
@@ -0,0 +1,18 @@
+const number10 = 10;
+const number20 = 20;
+
+
+const plus = number10 + number20;
+const minus = number10 - number20;
+const multiple = number10 * number20;
+const divide = number10 / number20;
+const divide2 = number20 % number10;
+
+
+console.log('10 + 20 = ', plus);
+console.log('10 - 20 = ', minus);
+console.log('10 * 20 = ', multiple);
+console.log('10 / 20 = ', divide);
+console.log('20 % 10 = ', divide2);
+console.log('5 % 3 = ', 5 % 3);
+
