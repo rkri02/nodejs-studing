993ec4f71cda25c85b7b8e6d05d6b9d8c0fa62a4
rkri02 <rkri02@naver.com>
2021-10-11 16:52:35 +0900

    기본적인 swutch - case문

---
+++ ex7-1.js
@@ -0,0 +1,18 @@
+const num=1;
+ 
+
+//기본적인 switch - case 문이다.
+switch(num)
+{
+    case 1:
+        console.log("예금 조회선택");break;
+    case 2:
+        console.log("예금 출금선택");break;
+    case 3:
+        console.log("예금 입금선택");break; 
+    case 4:
+        console.log("예금 이체선택");break;
+    default:
+        console.log("잘못 누르셨습니다");   
+} // switch문을 닫아줌
+
