904ff96077ba74ab1aa5cc47ff6021de05870f4d
rkri02 <rkri02@naver.com>
2021-10-11 14:41:37 +0900

    배열의 값을 찍기 위한 코드

---
+++ ex5-3.js
@@ -0,0 +1,16 @@
+const Arr = [];
+//배열의 첫번쨰를 찍기위한 과정이다
+console.log(Arr[0]); 
+
+//배열의 길이를 찍는 과정이지만 배열이 없어서 0이 나온다.
+console.log(Arr.length); 
+
+//push를 사용해서 배열을 추가한다
+Arr.push('첫째', '둘째', '셋째');
+
+//추가한 배열때문에 결과값은 3이 나오며
+console.log(Arr.length);
+
+//첫번쨰의 값을 찍기위한 과정이며 결과값은 '첫째'가 나온다
+console.log(Arr[0]); 
+
