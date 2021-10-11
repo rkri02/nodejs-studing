4254af247ac176b36b243cd16a974f5d9b247fd6
rkri02 <rkri02@naver.com>
2021-10-11 14:43:08 +0900

    typeof()로 타입을 찍는 코드

---
+++ ex5-5.js
@@ -0,0 +1,10 @@
+const PI = 3.14;
+
+//타입을 찍는 것으로 typeof()를 사용한다.
+console.log('Have a nice day : %s', typeof( 'hello'));
+console.log('"100" : %s', typeof('100'));
+console.log('pi : %s', typeof(PI));
+console.log('50 : %s', typeof(50));
+console.log('[] : %s', typeof([]));
+console.log('{} : %s', typeof({}));
+
