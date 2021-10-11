b69766b594fa99bb7aa1ad4fc22ccf704bfc3d3e
rkri02 <rkri02@naver.com>
2021-10-11 10:02:32 +0900

    isNAN() 메소드에 관한 간단한 설명

---
+++ ex3-4.js
@@ -0,0 +1,16 @@
+// isNAN() 메소드는 숫자인지 아닌지 판별해준다. 
+//값은 숫자가 아니면 true / 숫자이면 false로 반환
+// isNAN(데이터) 형식으로 들어간다
+
+
+const isNaN1 = isNaN(100);
+const isNaN2 = isNaN(10.1);
+const isNaN3 = isNaN('12345');
+const isNaN4 = isNaN('nice');
+
+
+
+console.log('isNaN1:', isNaN1);
+console.log('isNaN2:', isNaN2);
+console.log('isNaN3:', isNaN3);
+console.log('isNaN4:', isNaN4);
