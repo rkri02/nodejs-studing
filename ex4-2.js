3cb67b67bb4a3129f5753062700992e6c990480f
rkri02 <rkri02@naver.com>
2021-10-11 13:29:06 +0900

    기본적인 연산코드2

---
+++ ex4-2.js
@@ -0,0 +1,21 @@
+let num = 10;
+
+//값이 없으니 10
+console.log('number : ', num);
+
+//10을 더해줘서 값이 20
+num += 10;
+console.log('num += 10 : ', num);
+
+//10을 빼줘서 값이 10
+num -= 10;
+console.log('num -= 10 : ', num);
+
+//10을 곱해줘서 값이 100
+num *= 10;
+console.log('num *= 10 : ', num);
+
+//10을 나누어줘서 값이 10
+num /= 10;
+console.log('num /= 10 : ', num);
+
