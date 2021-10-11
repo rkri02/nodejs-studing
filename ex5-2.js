9c3874ef33278879d12456d849615a9891bd8d12
rkri02 <rkri02@naver.com>
2021-10-11 14:40:53 +0900

    .push()로 배열을 추가

---
+++ ex5-2.js
@@ -0,0 +1,16 @@
+// 배열은 .push()로 배열을 추가할수 있다.
+const Num = [];
+Num.push(11);
+Num.push(22);
+Num.push(33);
+
+
+const Strings = [];
+Strings.push('hello', 'welcome', 'bye');
+
+
+// 배열을 추가해서 각각 [11,22,33]
+// ['hello','welcome','bye']로 나타내진다.
+console.log(Num);
+console.log(Strings);
+
