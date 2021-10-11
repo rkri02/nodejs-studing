bdde4765118ad5b7bad8807b07775c3f70bc2e45
rkri02 <rkri02@naver.com>
2021-10-11 14:40:19 +0900

    배열을 위한 기본설명

--- ex4-5.js
+++ ex4-5.js
@@ -3,22 +3,23 @@
 // 조건이 참일경우 수식1이 결과값 거짓일경우 수식2가 결과값을 가진다.
 
 
-let x = 10;  
-let y = 20;
-let z = ( x > y ? x : y);
+let x = 10;  
+let y = 20;
+let z = ( x > y ? x : y);
 
 
 console.log(x);
 console.log(y);
 console.log(z);
+// 각각의 출력되는 문자는 10,20,20으로 순서대로 출력이 된다.
 
-
-//문자열로 표현해서 쉽게 확인하기 위함
-let x1 = "true";  
-let y1 = "false";
-let zt = ( x < y ? x1 : y1);
-let zf = ( x > y ? x1 : y1);
+// 문자열로 표현해서 쉽게 확인하기 위함
+let x1 = "true";  
+let y1 = "false";
+let zt = ( x < y ? x1 : y1);
+let zf = ( x > y ? x1 : y1);
 
 
 console.log(zt);
 console.log(zf);
+// 각각의 출력되는 문자는 true / false로 나타나 진다
---
+++ ex5-1.js
@@ -0,0 +1,15 @@
+//숫자 배열과 문자열 배열을 각각 나타낸다.
+const numbers = [10, 20, 30];
+const strings = ['Have', 'a', 'nice', 'Day'];
+
+
+//new 라는 연산자를 이용해서 배열 문자열 숫자로 각각 선언한다.
+const numbers2 = new Array(10, 20, 30);
+const strings2 = new Array('Have', 'a','nice', 'Day');
+
+
+console.log('numbers : ', numbers);
+console.log('numbers2 : ', numbers2);
+console.log('strings : ', strings);
+console.log('strings2 : ', strings2);
+
