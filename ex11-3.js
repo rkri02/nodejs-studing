1c002da962a1e7f0c8def5e6031f3440ad1e7363
rkri02 <rkri02@naver.com>
2021-10-12 23:40:06 +0900

    전역객체에 대한 설명

---
+++ ex11-3.js
@@ -0,0 +1,16 @@
+// process
+console.log(process.arch); // 프로세서의 아키텍쳐
+console.log("===================");
+console.log(process.memoryUsage());
+
+
+ // 메모리 사용 정보를 가진 객체
+console.log("===================");
+console.log(process.version); // Node.js 버전
+console.log("===================");
+
+
+// console
+console.log('파일명 : ', __filename);
+console.log('경로명 : ', __dirname);
+
