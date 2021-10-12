0961951a62e5dd98394fdc84aa88fab223d05913
rkri02 <88758071+rkri02@users.noreply.github.com>
2021-10-12 23:50:34 +0900

    기본적인 함수처리

---
+++ ex12-1.js
@@ -0,0 +1,23 @@
+function Hello()
+{
+    console.log("Hello");
+}
+function Hi()
+{
+    console.log("Hi");
+}
+function Morning()
+{
+    console.log("Good Morning~");
+}
+function Bye()
+{
+    console.log("Bye");
+}
+
+
+Hello();
+Hi();
+Morning();
+Bye();
+
---
+++ ex12-2.js
@@ -0,0 +1,10 @@
+function Max(x,y)
+{     
+    if (x>y)
+        console.log("%d가 더크다",x);
+    else if( y>x)
+        console.log("%d가 더크다",y);
+}
+
+//매개변수가 없을 수도 있고, 여러 개 올 수도 있다.
+Max(10,20);
