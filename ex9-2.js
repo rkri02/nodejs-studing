e5be24d1d9c28e8da72180f6ae6f20811a35d4b3
rkri02 <rkri02@naver.com>
2021-10-12 23:23:58 +0900

    기본적인 while문

---
+++ ex9-2.js
@@ -0,0 +1,9 @@
+//let으로 선언해도 가능하다.
+var fruit = ['사과', '바나나', '오렌지'];
+
+
+//forEach 는 오적 array객체에서만 사용가능
+fruit.forEach(function(item) 
+{
+    console.log(item);
+});
---
+++ ex9-3.js
@@ -0,0 +1,16 @@
+let cnt = 1;   
+       
+while(cnt <= 5)
+{      
+    console.log("Nice !"); 
+    cnt += 1 ;       
+}  
+
+var n = 1 ;
+while( 1 )
+{               
+    console.log("안녕하세요! ") ; 
+    if( n >= 5 ) break;
+    n += 1;      
+
+} 
