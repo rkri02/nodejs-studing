788eb3ae6f5e06592bcdaf47156ffc04cd3ddf3f
rkri02 <rkri02@naver.com>
2021-10-13 11:51:18 +0900

    여러함수에 대한 설명

---
+++ ex19-2.js
@@ -0,0 +1,11 @@
+var beforeStr = "010-1234-5678";
+var afterStr = beforeStr.split('-');
+
+
+console.log(afterStr[0]);
+console.log(afterStr[1]);
+console.log(afterStr[2]);
+
+/*==========================================
+split()은 문자열을 분할하는 메서드입니다.
+==========================================*/
---
+++ ex19-3.js
@@ -0,0 +1,17 @@
+const string1 = 'Nice to meet you';
+const string2 = 'Hello World';
+const string3 = 'Good to see you';
+
+
+const substring1 = string1.substring(0, 4);
+const substring2 = string2.substring(6, 9);
+const substring3 = string3.substring(8, 11);
+
+
+console.log(substring1);
+console.log(substring2);
+console.log(substring3);
+
+/*================================================================
+substring()은 문자열에서 특정 부분만 골라낼 때 사용하는 메서드입니다.
+================================================================*/
