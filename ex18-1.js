d15d1d866a917c624d8ce68496d343fa78fe40d4
rkri02 <rkri02@naver.com>
2021-10-13 11:37:27 +0900

    floor 함수에 대한 설명

---
+++ ex18-1.js
@@ -0,0 +1,14 @@
+const floor1 = Math.floor(49.95);       // 49
+const floor2 = Math.floor(5);           // 5
+const floor3 = Math.floor(NaN);         // NaN
+const floor4 = Math.floor(-45.05);      // -46
+
+
+console.log('floor1:', floor1);
+console.log('floor2:', floor2);
+console.log('floor3:', floor3);
+console.log('floor4:', floor4);
+
+/*===================================================================
+Math.floor 는 소수 값이 존재할 때 소수 값을 버리는 역할을 하는 함수이다.
+===================================================================*/
