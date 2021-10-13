94b1bd42078a5c0e79b87435801b0af4712e8778
rkri02 <rkri02@naver.com>
2021-10-13 11:38:11 +0900

    ceil 함수(소수올림)에 대한 설명

---
+++ ex18-2.js
@@ -0,0 +1,14 @@
+const ceil1 = Math.ceil(5.004);         //  5
+const ceil2 = Math.ceil(-0.97);         // -0
+const ceil3 = Math.ceil(-4);            // -4
+const ceil4 = Math.ceil(-8.004);        // -8
+
+
+console.log('ceil1:', ceil1);
+console.log('ceil2:', ceil2);
+console.log('ceil3:', ceil3);
+console.log('ceil4:', ceil4);
+
+/*=============================================================
+Math.ceil 은 소수 값이 존재할 때 값을 올리는 역할을 하는 함수이다.
+=============================================================*/
