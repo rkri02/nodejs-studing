caf5f869f426696c30e172492810b5465d86a77d
rkri02 <rkri02@naver.com>
2021-10-13 09:48:06 +0900

    round()함수에 대한 설명

---
+++ ex16-2.js
@@ -0,0 +1,12 @@
+const number = 77.8;
+const divide = 1 / 2;
+
+
+console.log('type:%s %d', typeof number, number);
+console.log('round', typeof number, Math.round(number));
+console.log('divide:%d, %d', divide, Math.round(divide));
+
+/*===================================================================
+1. round(숫자를) 반올림하다.
+2. 주어진 수를 가장 가까운 정수로 반올림하다. 
+===================================================================*/
